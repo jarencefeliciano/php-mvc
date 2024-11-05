062896d5e565f9823d03d08686eca97218088dc7
Jasper Lawrence Feliciano <jarence.feliciano@gmail.com>
2024-11-06 00:18:44 +0800

    Add show a message if no route matches.

--- public/index.php
+++ public/index.php
@@ -12,6 +12,10 @@ $router->add("/", ["controller" => "home", "action" => "index"]);
 
 $params = $router->match($path);
 
+if ($params === false) {
+    exit("No route matched.");
+}
+
 $controller = $params["controller"];
 $action = $params["action"];
 
