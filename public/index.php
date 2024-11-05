4469cd6b1679ab39c6fd52f5b37e3a1113be92e1
Jasper Lawrence Feliciano <jarence.feliciano@gmail.com>
2024-11-05 20:01:26 +0800

    Format the data using HTML.

--- public/index.php
+++ public/index.php
@@ -10,4 +10,24 @@ $stmt = $pdo->query("SELECT * FROM products");
 
 $products = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-print_r($products);
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>PHP MVC</title>
+</head>
+<body>
+
+<h1>Products</h1>
+
+<?php foreach ($products as $product): ?>
+
+  <h2><?= htmlspecialchars($product["name"]) ?></h2>
+  <p><?= htmlspecialchars($product["description"]) ?></p>
+
+<?php endforeach; ?>
+
+</body>
+</html>
