7edcc5d13c051a22fd44df3e18cb11ac6cc401cf
Jasper Lawrence Feliciano <jarence.feliciano@gmail.com>
2024-11-05 19:37:03 +0800

    Retrieve data from the database using plain PHP.

--- public/index.php
+++ public/index.php
@@ -1,3 +1,13 @@
 <?php
 
-echo "Hello World";
+$dsn = "mysql:host=localhost;dbname=php_mvc;charset=utf8mb4;port=3306";
+
+$pdo = new PDO($dsn, "jarence", "@nd7srmu2!", [
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+]);
+
+$stmt = $pdo->query("SELECT * FROM products");
+
+$products = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+print_r($products);
