0ba548a98fd1c9303815478962a205bb1080419c
Jasper Lawrence Feliciano <jarence.feliciano@gmail.com>
2024-11-05 22:41:32 +0800

    Remove code repetition by dynamically creating objects and running methods.

--- public/index.php
+++ public/index.php
@@ -1,30 +1,10 @@
 <?php
 
-
-
 $controller = $_GET["controller"];
 $action = $_GET["action"];
 
-if ($controller === "products") {
+require "../src/controllers/$controller.php";
 
-  require "../src/controllers/products.php";
+$controller_object = new $controller;
 
-  $controller_object = new Products;
-
-} elseif ($controller === "home") {
-
-  require "../src/controllers/home.php";
-
-  $controller_object = new Home;
-
-}
-
-if ($action === "index") {
-
-  $controller_object->index();
-
-} elseif ($action === "show") {
-
-  $controller_object->show();
-
-}
+$controller_object->$action();
