f435ccd62bfff85fe341ed246d8a02fddceab8e5
Jasper Lawrence Feliciano <jarence.feliciano@gmail.com>
2024-11-05 23:46:29 +0800

    Add router class and load it in the front controller.

--- public/index.php
+++ public/index.php
@@ -2,6 +2,10 @@
 
 $path = parse_url($_SERVER["REQUEST_URI"], PHP_URL_PATH);
 
+require "../src/router.php";
+
+$router = new Router;
+
 $segments = explode("/", $path);
 
 $controller = $segments[1];
