d3a6850a14378e1191643585580f6c26df122566
Jasper Lawrence Feliciano <jarence.feliciano@gmail.com>
2024-11-06 21:42:25 +0800

    Add pattern to match the URL path instead of a fixed string.

--- src/Framework/Router.php
+++ src/Framework/Router.php
@@ -16,7 +16,7 @@ class Router
 
     public function match(string $path): array|bool
     {
-        $pattern = "#^/home/index$#";
+        $pattern = "#^/[a-z]+/[a-z]+$#";
 
         if (preg_match($pattern, $path)) {
             exit("Match");
