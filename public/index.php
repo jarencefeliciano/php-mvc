71a392c4132223ede72515fb5409985e13a0ca3b
Jasper Lawrence Feliciano <jarence.feliciano@gmail.com>
2024-11-07 20:17:00 +0800

    Process the route path one segment at a time.

--- public/index.php
+++ public/index.php
@@ -8,7 +8,7 @@ spl_autoload_register(function (string $class_name) {
 
 $router = new Framework\Router;
 
-$router->add("{controller}/{action}");
+$router->add("/{controller}/{action}");
 // $router->add("/home/index", ["controller" => "home", "action" => "index"]);
 // $router->add("/products", ["controller" => "products", "action" => "index"]);
 // $router->add("/", ["controller" => "home", "action" => "index"]);
--- src/Framework/Router.php
+++ src/Framework/Router.php
@@ -19,11 +19,22 @@ class Router
         foreach ($this->routes as $route) {
             $pattern = "#^/(?<controller>[a-z]+)/(?<action>[a-z]+)$#";
             echo $pattern, "\n", $route["path"], "\n";
+            $this->getPatternFromRoutePath($route["path"]);
             if (preg_match($pattern, $path, $matches)) {
                 $matches = array_filter($matches, "is_string", ARRAY_FILTER_USE_KEY);
                 return $matches;
             }
         }
         return false;
+    }
+
+    private function getPatternFromRoutePath(string $route_path)
+    {
+        $route_path = trim($route_path, "/");
+        $segments = explode("/", $route_path);
+        // $segments = array_map(function(string $segment): string {
+        //     return $segment;
+        // }, $segments);
+        print_r($segments);
     }
 }
