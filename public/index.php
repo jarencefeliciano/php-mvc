5fdf3acafe7bd149024f4e8eef2213bed7ae8a65
Jasper Lawrence Feliciano <jarence.feliciano@gmail.com>
2024-11-07 23:31:56 +0800

    Order the routes from specific ones to the generic ones.

--- public/index.php
+++ public/index.php
@@ -8,12 +8,13 @@ spl_autoload_register(function (string $class_name) {
 
 $router = new Framework\Router;
 
-// $router->add("/{controller}/{action}");
 $router->add("/product/{slug:[\w-]+}", ["controller" => "products", "action" => "show"]);
-$router->add("/{controller}/{action}/{id:\d+}");
 $router->add("/home/index", ["controller" => "home", "action" => "index"]);
 $router->add("/products", ["controller" => "products", "action" => "index"]);
 $router->add("/", ["controller" => "home", "action" => "index"]);
+
+$router->add("/{controller}/{action}/{id:\d+}");
+$router->add("/{controller}/{action}");
 
 $params = $router->match($path);
 
