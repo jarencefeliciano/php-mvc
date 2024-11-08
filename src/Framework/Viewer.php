d5cb18bf83c66901afaf4618e83cfaf221453387
Jasper Lawrence Feliciano <jarence.feliciano@gmail.com>
2024-11-08 08:42:59 +0800

    Use output buffering to load the contents of the template.

--- src/Framework/Viewer.php
+++ src/Framework/Viewer.php
@@ -4,10 +4,15 @@ namespace Framework;
 
 class Viewer
 {
-    public function render(string $template, array $data = [])
+    public function render(string $template, array $data = []): string
     {
         extract($data, EXTR_SKIP);
 
+        ob_start();
+
         require "../src/views/$template";
+        // return file_get_contents("../src/views/$template");
+
+        return ob_get_clean();
     }
 }
