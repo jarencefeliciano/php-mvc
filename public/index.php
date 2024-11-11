a89e7d4b6a3d89a67c9a222fbaae008b8d30663f
Jasper Lawrence Feliciano <jarence.feliciano@gmail.com>
2024-11-11 16:00:17 +0800

    Move the exception handler into a separate class.

--- public/index.php
+++ public/index.php
@@ -7,31 +7,7 @@ spl_autoload_register(function (string $class_name) {
 });
 
 set_error_handler("Framework\ErrorHandler::handleError");
-
-set_exception_handler(function (Throwable $exception) {
-
-    if ($exception instanceof Framework\Exceptions\PageNotFoundException) {
-        http_response_code(404);
-        $template = "404.php";
-    } else {
-        http_response_code(500);
-        $template = "500.php";
-    }
-
-    $show_erros = true;
-
-    if ($show_erros) {
-        ini_set("display_errors", "1");
-    } else {
-        ini_set("display_errors", "0");
-        ini_set("log_errors", "1");
-        // echo ini_get("error_log");
-        require "../src/App/views/$template";
-    }
-
-    throw $exception;
-});
-
+set_exception_handler("Framework\ErrorHandler::handleException");
 
 $path = parse_url($_SERVER["REQUEST_URI"], PHP_URL_PATH);
 
--- src/Framework/ErrorHandler.php
+++ src/Framework/ErrorHandler.php
@@ -4,7 +4,9 @@ declare(strict_types=1);
 
 namespace Framework;
 
+use Throwable;
 use ErrorException;
+use Framework\Exceptions\PageNotFoundException;
 
 class ErrorHandler
 {
@@ -16,5 +18,29 @@ class ErrorHandler
     ): bool
     {
         throw new ErrorException($errstr, 0, $errno, $errfile, $errline);
+    }
+
+    public static function handleException(Throwable $exception): void {
+
+        if ($exception instanceof PageNotFoundException) {
+            http_response_code(404);
+            $template = "404.php";
+        } else {
+            http_response_code(500);
+            $template = "500.php";
+        }
+
+        $show_erros = true;
+
+        if ($show_erros) {
+            ini_set("display_errors", "1");
+        } else {
+            ini_set("display_errors", "0");
+            ini_set("log_errors", "1");
+            // echo ini_get("error_log");
+            require "../src/App/views/$template";
+        }
+
+        throw $exception;
     }
 }
