750605f16fa9cf3b8f9aff537bb558fbef86c2a0
Jasper Lawrence Feliciano <jarence.feliciano@gmail.com>
2024-11-11 23:25:28 +0800

    Redisplay the form with the error messages for invalid data.

--- src/App/Controllers/Products.php
+++ src/App/Controllers/Products.php
@@ -71,8 +71,18 @@ class Products
             "description" => empty($_POST["description"]) ? null : $_POST["description"]
         ];
 
-        var_dump($this->model->insert($data));
+        if ($this->model->insert($data)) {
+            echo "record saved.";
+        } else {
+            echo $this->viewer->render("shared/header.php", [
+                "title" => "New Product"
+            ]);
 
-        print_r($this->model->getErrors());
+            echo $this->viewer->render("Products/new.php", [
+              "errors" => $this->model->getErrors()
+            ]);
+
+            echo $this->viewer->render("shared/footer.php");
+        }
     }
 }
