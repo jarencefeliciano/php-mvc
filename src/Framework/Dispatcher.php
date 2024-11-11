b85ec9a075fb0bbcff6485695122b884d5183132
Jasper Lawrence Feliciano <jarence.feliciano@gmail.com>
2024-11-11 12:02:21 +0800

    Thrown a custom exception if there is no match for the URL path.

--- src/Framework/Dispatcher.php
+++ src/Framework/Dispatcher.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Framework;
 
 use ReflectionMethod;
+use Framework\Exceptions\PageNotFoundException;
 
 class Dispatcher
 {
@@ -18,7 +19,7 @@ class Dispatcher
         $params = $this->router->match($path);
 
         if ($params === false) {
-            exit("No route matched.");
+            throw new PageNotFoundException("No route matched for '$path'.");
         }
 
         $dependencies = [];
