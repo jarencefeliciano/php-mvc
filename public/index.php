19278d17ebf80831303fc98f13b81ac752905246
Jasper Lawrence Feliciano <jarence.feliciano@gmail.com>
2024-11-11 16:03:55 +0800

    Move the routes to a separate configuration file.

--- public/index.php
+++ public/index.php
@@ -15,17 +15,7 @@ if ($path === false) {
     throw new UnexpectedValueException("Malformed URL: '{$_SERVER["REQUEST_URI"]}'");
 }
 
-$router = new Framework\Router;
-
-$router->add("/admin/{controller}/{action}", ["namespace" => "Admin"]);
-$router->add("/{title}/{id:\d+}/{page:\d+}", ["controller" => "products", "action" => "showPage"]);
-$router->add("/product/{slug:[\w-]+}", ["controller" => "products", "action" => "show"]);
-$router->add("/home/index", ["controller" => "home", "action" => "index"]);
-$router->add("/products", ["controller" => "products", "action" => "index"]);
-$router->add("/", ["controller" => "home", "action" => "index"]);
-
-$router->add("/{controller}/{action}/{id:\d+}");
-$router->add("/{controller}/{action}");
+$router = require "../src/config/routes.php";
 
 $container = new Framework\Container;
 
