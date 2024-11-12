3de7a58743183cef7031b700d3f74bc6da47b252
Jasper Lawrence Feliciano <jarence.feliciano@gmail.com>
2024-11-13 01:08:53 +0800

    Replace PHP variables with the alternative variable syntax.

--- src/App/Views/Products/index.mvc.php
+++ src/App/Views/Products/index.mvc.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title><?= $title ?></title>
+  <title>{{ title }}</title>
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/water.css@2/out/water.css">
 </head>
 <body>
@@ -13,13 +13,13 @@
 
 <a href="/products/new">New Product</a>
 
-<p>Total: <?= $total ?></p>
+<p>Total: {{ total }}</p>
 
 <?php foreach ($products as $product): ?>
 
 <h2>
-    <a href="/products/show/<?= $product["id"] ?>">
-        <?= htmlspecialchars($product["name"]) ?>
+    <a href="/products/show/{{ product["id"] }}">
+        {{ product["name"] }}
     </a>
 </h2>
 
--- src/Framework/MVCTemplateViewer.php
+++ src/Framework/MVCTemplateViewer.php
@@ -8,12 +8,23 @@ class MVCTemplateViewer implements TemplateViewerInterface
 {
     public function render(string $template, array $data = []): string
     {
-        extract($data, EXTR_SKIP);
+        $code = file_get_contents(__DIR__ . '/../App/Views/' . $template);
 
-        ob_start();
+        $code = $this->replaceVariables($code);
 
-        require_once __DIR__ . '/../App/Views/' . $template;
+        return $code;
 
-        return ob_get_clean();
+        // extract($data, EXTR_SKIP);
+
+        // ob_start();
+
+        // require_once __DIR__ . '/../App/Views/' . $template;
+
+        // return ob_get_clean();
+    }
+
+    private function replaceVariables(string $code): string
+    {
+        return preg_replace("#{{\s*(\S+)\s*}}#", "<?= htmlspecialchars(\$$1) ?>", $code);
     }
 }
