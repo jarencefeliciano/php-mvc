9aceed01b19cfabf61ebb1e9d8ef7ebeef4427e6
Jasper Lawrence Feliciano <jarence.feliciano@gmail.com>
2024-11-12 13:38:20 +0800

    Replace the generic route with an ID with specific routes.

--- src/config/routes.php
+++ src/config/routes.php
@@ -2,12 +2,19 @@
 
 $router = new Framework\Router;
 
+// Specified routes for CRUD
 $router->add("/admin/{controller}/{action}", ["namespace" => "Admin"]);
 $router->add("/{title}/{id:\d+}/{page:\d+}", ["controller" => "products", "action" => "showPage"]);
 $router->add("/product/{slug:[\w-]+}", ["controller" => "products", "action" => "show"]);
 $router->add("/home/index", ["controller" => "home", "action" => "index"]);
 $router->add("/products", ["controller" => "products", "action" => "index"]);
 $router->add("/", ["controller" => "home", "action" => "index"]);
+
+$router->add("/{controller}/show/{id:\d+}", ["action" => "show"]);
+$router->add("/{controller}/edit/{id:\d+}", ["action" => "edit"]);
+$router->add("/{controller}/update/{id:\d+}", ["action" => "update"]);
+$router->add("/{controller}/delete/{id:\d+}", ["action" => "delete"]);
+$router->add("/{controller}/destroy/{id:\d+}", ["action" => "destroy"]);
 
 $router->add("/{controller}/{action}/{id:\d+}");
 $router->add("/{controller}/{action}");
