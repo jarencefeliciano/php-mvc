df843f3e166a7a5a525a484ae6122723a0e3b0ac
Jasper Lawrence Feliciano <jarence.feliciano@gmail.com>
2024-11-13 20:20:14 +0800

    Define an interface for handling a request.

--- src/App/Middleware/ChangeResponseExample.php
+++ src/App/Middleware/ChangeResponseExample.php
@@ -6,10 +6,11 @@ namespace App\Middleware;
 
 use Framework\Request;
 use Framework\Response;
+use Framework\RequestHandlerInterface;
 
 class ChangeResponseExample
 {
-    public function process (Request $request, $next): Response
+    public function process (Request $request, RequestHandlerInterface $next): Response
     {
         $response = $next->handle($request);
 
